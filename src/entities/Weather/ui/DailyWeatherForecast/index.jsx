4a6c5d45bd8fb871d089e67f67c821867a558f48
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2025-03-06 17:48:01 +0400

    Upgrade

--- src/entities/Weather/ui/DailyWeatherForecast/index.jsx
+++ src/entities/Weather/ui/DailyWeatherForecast/index.jsx
@@ -15,51 +15,45 @@ export const DailyWeatherForecast = ({
       new Date(forecastData?.list[0]?.dt_txt)?.getHours(),
   )
 
-  return isLoading
-    ? null
-    : fiveDays?.map((day, i) => {
-        const time = formatTime(day.dt)
-        const temp = Math.round(day.main.temp)
-        const unitSymbol = units === 'metric' ? 'C' : 'F'
-        const weekDay = new Date(day.dt_txt).getDay()
+  if (isLoading) return null
 
-        const weekDays = {
-          1: 'Monday',
-          2: 'Tuesday',
-          3: 'Wednesday',
-          4: 'Thursday',
-          5: 'Friday',
-          6: 'Saturday',
-          0: 'Sunday',
-        }
+  fiveDays?.map((day, i) => {
+    const time = formatTime(day.dt)
+    const temp = Math.round(day.main.temp)
+    const unitSymbol = units === 'metric' ? 'C' : 'F'
+    const weekDay = new Date(day.dt_txt).getDay()
 
-        const activeDay = day?.dt === currentDay?.dt ? 'activeDay' : ''
+    const weekDays = {
+      1: 'Monday',
+      2: 'Tuesday',
+      3: 'Wednesday',
+      4: 'Thursday',
+      5: 'Friday',
+      6: 'Saturday',
+      0: 'Sunday',
+    }
 
-      console.log(currentDay)
+    const activeDay = day?.dt === currentDay?.dt ? 'activeDay' : ''
 
-        return (
-          <div
-            key={i}
-            className={clsx(
-              styles.daily,
-              styles[`div${i + 8}`],
-              styles[activeDay],
-            )}
-            onClick={() => handleClickDay(day)}
-          >
-            <div className={styles.time}>
-              {weekDays[weekDay]} {time}
-            </div>
-            <div className={styles.temp}>
-              {temp}°{unitSymbol}
-            </div>
-            <div>
-              <img
-                src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                alt=""
-              />
-            </div>
-          </div>
-        )
-      })
+    return (
+      <div
+        key={i}
+        className={clsx(styles.daily, styles[`div${i + 8}`], styles[activeDay])}
+        onClick={() => handleClickDay(day)}
+      >
+        <div className={styles.time}>
+          {weekDays[weekDay]} {time}
+        </div>
+        <div className={styles.temp}>
+          {temp}°{unitSymbol}
+        </div>
+        <div>
+          <img
+            src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+            alt=""
+          />
+        </div>
+      </div>
+    )
+  })
 }
