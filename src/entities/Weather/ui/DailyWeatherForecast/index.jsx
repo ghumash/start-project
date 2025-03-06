d211b17683ed4773276a43c099d4985070a9763d
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2025-03-06 18:41:12 +0400

    Upgrade

--- src/entities/Weather/ui/DailyWeatherForecast/index.jsx
+++ src/entities/Weather/ui/DailyWeatherForecast/index.jsx
@@ -9,48 +9,50 @@ export const DailyWeatherForecast = ({
   isLoading,
   currentDay,
 }) => {
-  const fiveDays = forecastData?.list?.filter(
-    (i) =>
-      new Date(i.dt_txt)?.getHours() ===
-      new Date(forecastData?.list[0]?.dt_txt)?.getHours(),
-  )
-
   if (isLoading) return null
 
-  fiveDays?.map((day, i) => {
-    const time = formatTime(day.dt)
-    const temp = Math.round(day.main.temp)
+  const fiveDays = forecastData?.list?.filter(
+    (i) =>
+      new Date(i.dt_txt).getHours() ===
+      new Date(forecastData.list[0].dt_txt).getHours(),
+  )
+
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+
+  return fiveDays?.map((day, i) => {
+    const { dt, main, weather, dt_txt } = day
+    const time = formatTime(dt)
+    const temp = Math.round(main.temp)
     const unitSymbol = units === 'metric' ? 'C' : 'F'
-    const weekDay = new Date(day.dt_txt).getDay()
-
-    const weekDays = {
-      1: 'Monday',
-      2: 'Tuesday',
-      3: 'Wednesday',
-      4: 'Thursday',
-      5: 'Friday',
-      6: 'Saturday',
-      0: 'Sunday',
-    }
-
-    const activeDay = day?.dt === currentDay?.dt ? 'activeDay' : ''
+    const weekDay = weekDays[new Date(dt_txt).getDay()]
+    const isActive = dt === currentDay?.dt
 
     return (
       <div
-        key={i}
-        className={clsx(styles.daily, styles[`div${i + 8}`], styles[activeDay])}
+        key={dt}
+        className={clsx(styles.daily, styles[`div${i + 8}`], {
+          [styles.activeDay]: isActive,
+        })}
         onClick={() => handleClickDay(day)}
       >
         <div className={styles.time}>
-          {weekDays[weekDay]} {time}
+          {weekDay} {time}
         </div>
         <div className={styles.temp}>
           {temp}°{unitSymbol}
         </div>
         <div>
           <img
-            src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-            alt=""
+            src={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+            alt={weather[0].description}
           />
         </div>
       </div>
